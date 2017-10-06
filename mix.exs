1cb052e9b1b75a71d1fb574107cbb82f403714e1
Pieter Rijken <pieter.rijken@gmail.com>
2017-10-07 01:35:13 +0200

    Second commit

--- mix.exs
+++ mix.exs
@@ -7,7 +7,11 @@ defmodule Chi2fit.Mixfile do
       version: "0.2.0",
       elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
-      deps: deps()
+      deps: deps(),
+      ## Hex stuff:
+      description: description(),
+      package: package(),
+      name: "Chi-SquaredFit"
     ]
   end
 
@@ -25,4 +29,22 @@ defmodule Chi2fit.Mixfile do
       {:ex_doc, "~> 0.16", only: :dev, runtime: false}
     ]
   end
+  
+  # Hex Package Manager stuff:
+  defp description() do
+    """
+    Provides functions for fast matrix inversion, creation of empirical CDF from sample data including
+    handling of asymmetric errors, and fitting to a funtion using chi-squared. The fitting procedure return
+    the full covariance matrix describing the fitted parameters.
+    """
+  end
+  
+  defp package() do
+    [
+      maintainers: ["Pieter Rijken"],
+      licenses: ["Apache 2.0"],
+      files: ["lib", "mix.exs", "README*", "LICENSE*", "config"]
+    ]
+  end
+
 end
