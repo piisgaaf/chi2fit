62b6c22a0ff12f53a08bd66f133e6cd14abe8a72
Pieter Rijken <pieter.rijken@gmail.com>
2018-06-19 16:44:11 +0200

    fix wilson score

--- mix.exs
+++ mix.exs
@@ -19,7 +19,7 @@ defmodule Chi2fit.Mixfile do
   def project do
     [
       app: :chi2fit,
-      version: "0.8.3",
+      version: "0.8.4",
       elixir: "~> 1.6",
       start_permanent: Mix.env == :prod,
       deps: deps(),
