9de6ee4b1452bb9a43c3ccb291e7dadbc0bbf2a1
Pieter Rijken <pieter.rijken@gmail.com>
2019-05-21 00:09:55 +0200

    Fixed Erlang distribution

--- lib/distributions/erlang.ex
+++ lib/distributions/erlang.ex
@@ -20,10 +20,11 @@ defmodule Distribution.Erlang do
 
   alias Exboost.Math, as: M
   
-  defstruct [:pars]
+  defstruct [:pars,:batches]
   
   @type t() :: %__MODULE__{
-    pars: [number()]
+    pars: [number()],
+    batches: nil | pos_integer
   }
 
 end
@@ -42,10 +43,20 @@ defimpl Distribution, for: Distribution.Erlang do
   defp erlangCDF(k,lambda) when k<0 or lambda<0, do: raise ArithmeticError, "Erlang is only defined for positive shape and mode"
   defp erlangCDF(k,lambda) when k>0, do: &M.gamma_p(k,lambda*&1)
 
-  def skewness(%Erlang{pars: nil}), do: fn [k, _] -> 2/:math.sqrt(k) end
-  def kurtosis(%Erlang{pars: nil}), do: fn [k, _] -> 6/k end
-  def size(%Erlang{}), do: 2
-  def cdf(%Erlang{pars: nil}), do: fn x, [k, lambda] -> erlangCDF(k,lambda).(x) end
-  def pdf(%Erlang{pars: nil}), do: fn x, [k, lambda] -> :math.exp( -lambda*x + k*:math.log(lambda) + (k-1)*:math.log(x) - M.lgamma(k) ) end
-  def random(%Erlang{pars: [k, lambda]}), do: erlang(k,lambda).()
+  def skewness(%Erlang{pars: nil, batches: nil}), do: fn [k, _] -> 2/:math.sqrt(k) end
+  def skewness(%Erlang{pars: nil, batches: k}) when k>0, do: fn [_] -> 2/:math.sqrt(k) end
+
+  def kurtosis(%Erlang{pars: nil, batches: nil}), do: fn [k, _] -> 6/k end
+  def kurtosis(%Erlang{pars: nil, batches: k}) when k>0, do: fn [_] -> 6/k end
+
+  def size(%Erlang{batches: nil}), do: 2
+  def size(%Erlang{batches: k}) when k>0, do: 1
+
+  def cdf(%Erlang{pars: nil, batches: nil}), do: fn x, [k, lambda] -> erlangCDF(k,lambda).(x) end
+  def cdf(%Erlang{pars: nil, batches: k}) when k>0, do: fn x, [lambda] -> erlangCDF(k,lambda).(x) end
+  def pdf(%Erlang{pars: nil, batches: nil}), do: fn x, [k, lambda] -> :math.exp( -lambda*x + k*:math.log(lambda) + (k-1)*:math.log(x) - M.lgamma(k) ) end
+  def pdf(%Erlang{pars: nil, batches: k}) when k>0, do: fn x, [lambda] -> :math.exp( -lambda*x + k*:math.log(lambda) + (k-1)*:math.log(x) - M.lgamma(k) ) end
+
+  def random(%Erlang{pars: [k, lambda], batches: nil}), do: erlang(k,lambda).()
+  def random(%Erlang{pars: [lambda], batches: k}), do: erlang(k,lambda).()
 end
